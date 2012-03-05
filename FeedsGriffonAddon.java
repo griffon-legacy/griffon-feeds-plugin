0e5a851c40ac0854a6c9ec5bdfdac2cff9edd047
aalmiray <aalmiray@gmail.com>
2012-03-05 17:52:45 +0100

    Updates for Griffon 0.9.5

--- FeedsGriffonAddon.java
+++ FeedsGriffonAddon.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 the original author or authors.
+ * Copyright 2010-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,21 +14,16 @@
  * limitations under the License.
  */
 
+import griffon.util.ApplicationHolder;
+import griffon.plugins.feeds.FeedIconFactory;
+import org.codehaus.griffon.runtime.core.AbstractGriffonAddon;
+
 /**
- * @author Andres.Almiray
+ * @author Andres Almiray
  */
-
-// check to see if we already have a FeedsGriffonAddon
-boolean addonIsSet1
-builderConfig.each() { prefix, v ->
-    v.each { builder, views ->
-        addonIsSet1 = addonIsSet1 || 'FeedsGriffonAddon' == builder
+public class FeedsGriffonAddon extends AbstractGriffonAddon {
+    public FeedsGriffonAddon() {
+        super(ApplicationHolder.getApplication());
+        factories.put("feedIcon", new FeedIconFactory());
     }
-}
-
-if (!addonIsSet1) {
-    println 'Adding FeedsGriffonAddon to Builder.groovy'
-    builderConfigFile.append('''
-root.'FeedsGriffonAddon'.addon=true
-''')
 }
--- griffon-app/conf/BuildConfig.groovy
+++ griffon-app/conf/BuildConfig.groovy
@@ -2,10 +2,9 @@ griffon.project.dependency.resolution = {
     inherits "global"
     log "warn" 
     repositories {
-        griffonPlugins()
         griffonHome()
-        griffonCentral()
-        flatDir name: 'feedsPluginLib', dirs: 'lib'
+        String basePath = pluginDirPath? "${pluginDirPath}/" : ''
+        flatDir name: "feedsLibDir", dirs: ["${basePath}lib"]
         mavenRepo 'http://repository.sonatype.org/content/groups/public'
     }
     dependencies {
@@ -23,4 +22,16 @@ griffon {
     }
 }
 
-griffon.jars.destDir='target/addon'
+log4j = {
+    // Example of changing the log pattern for the default console
+    // appender:
+    appenders {
+        console name: 'stdout', layout: pattern(conversionPattern: '%d [%t] %-5p %c - %m%n')
+    }
+
+    error 'org.codehaus.griffon',
+          'org.springframework',
+          'org.apache.karaf',
+          'groovyx.net'
+    warn  'griffon'
+}
